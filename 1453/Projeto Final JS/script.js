16268459a192d69d55371ec0cadf62d7f04a7f12
Alexandre Miranda <ajlmiranda@hotmail.com>
2025-09-24 20:53:28 -0300

    projeto final JS basico

--- 1453/Projeto Final JS/script.js
+++ 1453/Projeto Final JS/script.js
@@ -1,65 +1,50 @@
-let listaDeMercado = [];
+const inputNovaTarefa = document.getElementById("addInput");
+const botaoAdicionar = document.getElementById("botaoAdicionar");
+const listaDeTarefas = document.getElementById("listarTarefas");
 
-function adicionar() {
-    const novoItem = prompt("Digite o novo item da lista de mercado");
-
-    listaDeMercado.push(novoItem);
-
-    menu();
-}
-function remover() {
-    const itemRemover = prompt("Digite o item para ser removido");
-
-    const listaFiltrada = listaDeMercado.filter((item) => item !== itemRemover);
-    listaDeMercado = listaFiltrada;
-
-    menu();
-}
-function editar() {
-    const paraEditar = prompt("Digite o nome do item para editar");
-    const novoNome = prompt("Digite o novo nome do item a ser editado");
-
-    const listaEditada = listaDeMercado.map((item) => {
-        if (item === paraEditar) {
-            return novoNome;
-        } else {
-            return item;
-        }
-    });
-    listaDeMercado = listaEditada;
-
-    menu();
-}
-function listar() {
-    console.log(listaDeMercado);
-
-    menu();
-}
-
-function menu() {
-    const promptOpcao = prompt(
-        "1. Adiciona 2.Remover 3.Editar 4.Listar 5.Sair"
-    );
-
-    switch (promptOpcao) {
-        case "1":
-            adicionar();
-            break;
-        case "2":
-            remover();
-            break;
-        case "3":
-            editar();
-            break;
-        case "4":
-            listar();
-            break;
-        case "5":
-            return;
-        default:
-            menu();
-            break;
+function adicionarTarefa() {
+    const inputText = inputNovaTarefa.value;
+    if (!inputText) {
+        console.log(inputText);
+        alert("Digite uma tarefa!");
+        return;
     }
+
+    const addNovaTarefa = document.createElement("li");
+    addNovaTarefa.classList.add("itemDaLista");
+    addNovaTarefa.textContent = inputText;
+    listaDeTarefas.appendChild(addNovaTarefa);
+    inputNovaTarefa.value = "";
+
+    const botaoDelete = document.createElement("button");
+    botaoDelete.classList.add("botaoDelete");
+    botaoDelete.textContent = "Excluir";
+    botaoDelete.onclick = function () {
+        listaDeTarefas.removeChild(addNovaTarefa);
+    };
+
+    addNovaTarefa.appendChild(botaoDelete);
+
+    const botaoEdit = document.createElement("button");
+    botaoEdit.classList.add("botaoEdit");
+    botaoEdit.textContent = "Editar";
+    botaoEdit.onclick = function () {
+        const itemEditado = prompt("Edite sua tarefa");
+        if (!itemEditado) {
+            alert("Sua tarefa não foi modificada!");
+            return;
+        }
+        addNovaTarefa.textContent = itemEditado;
+        addNovaTarefa.appendChild(botaoDelete);
+        addNovaTarefa.appendChild(botaoEdit);
+    };
+
+    addNovaTarefa.appendChild(botaoEdit);
 }
 
-menu();
+botaoAdicionar.addEventListener("click", adicionarTarefa);
+inputNovaTarefa.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        botaoAdicionar.click();
+    }
+});
